04b332638d63830dccb811304b52e016ba1bd09b
Morten Rasmussen <morten.rasmussen@arm.com>
2014-12-18 14:47:18 +0000

    ANDROID: sched: Calculate energy consumption of sched_group
    For energy-aware load-balancing decisions it is necessary to know the
    energy consumption estimates of groups of cpus. This patch introduces a
    basic function, sched_group_energy(), which estimates the energy
    consumption of the cpus in the group and any resources shared by the
    members of the group.
    
    NOTE: The function has five levels of identation and breaks the 80
    character limit. Refactoring is necessary.
    
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    
    BUG=chrome-os-partner:58667
    TEST=build/boot on kevin
    
    Change-Id: Ie5b4b7ee63f5a979a45404ffc48e5b5c2710fb3f
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    (cherry picked from commit c1770a521398102b6f2447502035887297b17ea
     from android-msm/android-4.4.y)
    Signed-off-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/420002
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6026,6 +6026,7 @@ DEFINE_PER_CPU(struct sched_domain *, sd_numa);
 DEFINE_PER_CPU(struct sched_domain *, sd_busy);
 DEFINE_PER_CPU(struct sched_domain *, sd_asym);
 DEFINE_PER_CPU(struct sched_domain *, sd_ea);
+DEFINE_PER_CPU(struct sched_domain *, sd_scs);
 
 static void update_top_cache_domain(int cpu)
 {
@@ -6059,6 +6060,9 @@ static void update_top_cache_domain(int cpu)
 			break;
 	}
 	rcu_assign_pointer(per_cpu(sd_ea, cpu), ea_sd);
+
+	sd = highest_flag_domain(cpu, SD_SHARE_CAP_STATES);
+	rcu_assign_pointer(per_cpu(sd_scs, cpu), sd);
 }
 
 /*

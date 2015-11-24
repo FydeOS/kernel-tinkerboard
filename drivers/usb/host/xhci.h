4768711b49260736d4cef31114428f4b2dc1a977
Chunfeng Yun <chunfeng.yun@mediatek.com>
2015-11-24 13:09:55 +0200

    UPSTREAM: xhci: mediatek: support MTK xHCI host controller
    There some vendor quirks for MTK xhci host controller:
    1. It defines some extra SW scheduling parameters for HW
      to minimize the scheduling effort for synchronous and
      interrupt endpoints. The parameters are put into reseved
      DWs of slot context and endpoint context.
    2. Its IMODI unit for Interrupter Moderation register is
      8 times as much as that defined in xHCI spec.
    3. Its TDS in  Normal TRB defines a number of packets that
      remains to be transferred for a TD after processing all
      Max packets in all previous TRBs.
    
    Signed-off-by: Chunfeng Yun <chunfeng.yun@mediatek.com>
    Tested-by: Daniel Thompson <daniel.thompson@linaro.org>
    Reviewed-by: Daniel Thompson <daniel.thompson@linaro.org>
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 0cbd4b34cda9dfd36b6c26b692dee181e0100b67)
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
    
    BUG=chrome-os-partner:41350
    TEST=test usbnet & udisk on oak-rev3
    
    Change-Id: Ib173d18b95a2ebe2f3ce0dea8d8a38f29a9d35ff
    Reviewed-on: https://chromium-review.googlesource.com/313060
    Reviewed-on: https://chromium-review.googlesource.com/423552
    Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1634,6 +1634,7 @@ struct xhci_hcd {
 /* For controllers with a broken beyond repair streams implementation */
 #define XHCI_BROKEN_STREAMS	(1 << 19)
 #define XHCI_PME_STUCK_QUIRK	(1 << 20)
+#define XHCI_MTK_HOST		(1 << 21)
 #define XHCI_MISSING_CAS	(1 << 24)
 #define XHCI_WARM_RESET_ON_RESUME	(1 << 25)
 	unsigned int		num_active_eps;

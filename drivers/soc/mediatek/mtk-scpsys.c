222755ff47095d41646a2105829442ab4fbdaf66
Sascha Hauer <s.hauer@pengutronix.de>
2015-11-30 13:31:08 +0100

    CHROMIUM: soc: mediatek: SCPSYS: Drop parent/child relationship for mfg domains
    The mfg power domains do not have a real parent/child relationship, so
    the current implementation does not reflect reality. Nevertheless the
    different mfg domains have hardware constraints in that they must be
    enabled and disabled in a certain order.
    To make that sure we no longer register the mfg_2d and mfg_async as
    domains themselves but instead only the mfg domain which then also
    enables/disabled the other two domains.
    
    Note: This is a non-upstreamable CHROMIUM patch, because while it is
    assumed that this is actually a work-around for an as-yet undiagnosed
    power sequencing issue in the drm/img-rogue GPU driver.
    An upstream solution would allow a fully functioning GPU driver to
    also operate in the following "intermediate" power states:
     (a) mfg_async
     (b) mfg_async + mfg_2d
    
    This fix in this patch disables these two modes since it always
    enables/disables all three power domains together, mfg_async/mfg_2d/mfg.
    
    For chromeos-4.4, this is a squash of:
    4762f11e0c95 CHROMIUM: soc: mediatek: SCPSYS: Drop parent/child relationship for mfg domains
    537dccbc8eee BACKPORT: soc: mediatek: SCPSYS: Fix double enabling of regulators
                 CHROMIUM: soc: mediatek: SCPSYS: Fix power off sequence in error path
     (https://chromium-review.googlesource.com/#/c/448737/)
    
    The BACKPORT was actually reverted upstream, since it really just
    fixes an implementation detail of the CHROMIUM patch - mainline
    does not have mfg domains, and hence doesn't have a 'double enabling issue'.
    
    The BACKPORT's commit message:
    
    With CONFIG_PM enabled do not call genpd->power_on manually as this
    will cause the regulators being turned on once in SCPSYS probe and
    then again when the genpd core turns on the domains. Instead, call
    genpd->power_on only with CONFIG_PM disabled and tell the genpd core
    that the domains are disabled when registered.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
    
    BUG=chrome-os-partner:45713
    TEST=build and boot oak-rev4
    TEST=Let system idle over night
     => No messages like:
        "mtk-scpsys 10006000.scpsys: Failed to power off domain mfg"
    TEST=Run power_LoadTest
     => No messages like:
        "mtk-scpsys 10006000.scpsys: Failed to power off domain mfg"
    BUG=chrome-os-partner:49416
    TEST=On oak-rev4:
        stop ui
        localhost ~ # cat /sys/kernel/debug/pm_genpd/pm_genpd_summary
        domain                          status          slaves
            /device                                             runtime status
            ----------------------------------------------------------------------
            mfg                             off
        localhost ~ # cat /sys/class/regulator/regulator.10/name
        vgpu
        localhost ~ # cat /sys/class/regulator/regulator.10/status
        off
    
    Reviewed-on: https://chromium-review.googlesource.com/317710
    Reviewed-on: https://chromium-review.googlesource.com/322044
    Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    
    Change-Id: Id53ef23b0ec215a57c7ae1c9d17dc89b63baea33
    Reviewed-on: https://chromium-review.googlesource.com/423591
    Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- drivers/soc/mediatek/mtk-scpsys.c
+++ drivers/soc/mediatek/mtk-scpsys.c
@@ -397,6 +397,46 @@ out:
 	return ret;
 }
 
+static int scpsys_mfg_power_on(struct generic_pm_domain *genpd)
+{
+	struct scp_domain *scpd = container_of(genpd, struct scp_domain, genpd);
+	struct scp *scp = scpd->scp;
+	int ret;
+
+	ret = scpsys_power_on(&scp->domains[MT8173_POWER_DOMAIN_MFG_ASYNC].genpd);
+	if (ret)
+		return ret;
+
+	ret = scpsys_power_on(&scp->domains[MT8173_POWER_DOMAIN_MFG_2D].genpd);
+	if (ret)
+		goto failed_mfg_2d;
+
+	ret = scpsys_power_on(&scp->domains[MT8173_POWER_DOMAIN_MFG].genpd);
+	if (ret)
+		goto failed_mfg;
+
+	return 0;
+
+failed_mfg:
+	scpsys_power_off(&scp->domains[MT8173_POWER_DOMAIN_MFG_2D].genpd);
+failed_mfg_2d:
+	scpsys_power_off(&scp->domains[MT8173_POWER_DOMAIN_MFG_ASYNC].genpd);
+
+	return ret;
+}
+
+static int scpsys_mfg_power_off(struct generic_pm_domain *genpd)
+{
+	struct scp_domain *scpd = container_of(genpd, struct scp_domain, genpd);
+	struct scp *scp = scpd->scp;
+
+	scpsys_power_off(&scp->domains[MT8173_POWER_DOMAIN_MFG].genpd);
+	scpsys_power_off(&scp->domains[MT8173_POWER_DOMAIN_MFG_2D].genpd);
+	scpsys_power_off(&scp->domains[MT8173_POWER_DOMAIN_MFG_ASYNC].genpd);
+
+	return 0;
+}
+
 static bool scpsys_active_wakeup(struct device *dev)
 {
 	struct generic_pm_domain *genpd;
@@ -486,19 +526,28 @@ static int scpsys_probe(struct platform_device *pdev)
 			scpd->clk[j] = clk[data->clk_id[j]];
 
 		genpd->name = data->name;
-		genpd->power_off = scpsys_power_off;
-		genpd->power_on = scpsys_power_on;
+		if (i == MT8173_POWER_DOMAIN_MFG) {
+			genpd->power_off = scpsys_mfg_power_off;
+			genpd->power_on = scpsys_mfg_power_on;
+		} else {
+			genpd->power_off = scpsys_power_off;
+			genpd->power_on = scpsys_power_on;
+		}
+
 		genpd->dev_ops.active_wakeup = scpsys_active_wakeup;
 
 		/*
-		 * Initially turn on all domains to make the domains usable
-		 * with !CONFIG_PM and to get the hardware in sync with the
-		 * software.  The unused domains will be switched off during
-		 * late_init time.
+		 * With CONFIG_PM disabled turn on all domains to make the
+		 * hardware usable.
 		 */
-		genpd->power_on(genpd);
+		if (!IS_ENABLED(CONFIG_PM))
+			genpd->power_on(genpd);
 
-		pm_genpd_init(genpd, NULL, false);
+		if (i == MT8173_POWER_DOMAIN_MFG_2D ||
+		    i == MT8173_POWER_DOMAIN_MFG_ASYNC)
+			continue;
+
+		pm_genpd_init(genpd, NULL, true);
 	}
 
 	/*
@@ -506,16 +555,6 @@ static int scpsys_probe(struct platform_device *pdev)
 	 * a power domain. Once registered above we have to keep the domains
 	 * valid.
 	 */
-
-	ret = pm_genpd_add_subdomain(pd_data->domains[MT8173_POWER_DOMAIN_MFG_ASYNC],
-		pd_data->domains[MT8173_POWER_DOMAIN_MFG_2D]);
-	if (ret && IS_ENABLED(CONFIG_PM))
-		dev_err(&pdev->dev, "Failed to add subdomain: %d\n", ret);
-
-	ret = pm_genpd_add_subdomain(pd_data->domains[MT8173_POWER_DOMAIN_MFG_2D],
-		pd_data->domains[MT8173_POWER_DOMAIN_MFG]);
-	if (ret && IS_ENABLED(CONFIG_PM))
-		dev_err(&pdev->dev, "Failed to add subdomain: %d\n", ret);
 
 	ret = of_genpd_add_provider_onecell(pdev->dev.of_node, pd_data);
 	if (ret)

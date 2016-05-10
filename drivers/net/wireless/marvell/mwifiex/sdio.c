ddee1986fcd741af6057a963a4e7a73d19e73a5e
Wei-Ning Huang <wnhuang@chromium.org>
2016-05-10 14:21:23 +0800

    UPSTREAM: mwifiex: fixup error messages
    Use dev_err instead of pr_err and add newline character at the end.
    
    Signed-off-by: Wei-Ning Huang <wnhuang@chromium.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    (cherry picked from commit 50d4d8feb9133abb5b8fcb22c48ec509295fdc43)
    
    BUG=b:36466135
    TEST=check wifi still works on kevin
    
    Change-Id: Iff05e3b6a6049d84bc7b6e5c89840558724c49bb
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/461858
    Tested-by: Brian Norris <briannorris@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>

--- drivers/net/wireless/marvell/mwifiex/sdio.c
+++ drivers/net/wireless/marvell/mwifiex/sdio.c
@@ -107,7 +107,7 @@ static int mwifiex_sdio_probe_of(struct device *dev, struct sdio_mmc_card *card)
 
 	if (!dev->of_node ||
 	    !of_match_node(mwifiex_sdio_of_match_table, dev->of_node)) {
-		pr_err("sdio platform data not available");
+		dev_err(dev, "sdio platform data not available\n");
 		return -1;
 	}
 
@@ -119,7 +119,8 @@ static int mwifiex_sdio_probe_of(struct device *dev, struct sdio_mmc_card *card)
 		cfg->dev = dev;
 		cfg->irq_wifi = irq_of_parse_and_map(card->plt_of_node, 0);
 		if (!cfg->irq_wifi) {
-			dev_err(dev, "fail to parse irq_wifi from device tree");
+			dev_err(dev,
+				"fail to parse irq_wifi from device tree\n");
 		} else {
 			ret = devm_request_irq(dev, cfg->irq_wifi,
 					       mwifiex_wake_irq_wifi,

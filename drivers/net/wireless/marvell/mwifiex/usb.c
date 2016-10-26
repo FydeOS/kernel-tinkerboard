d8fae2a7a917be8af20b5e4f31dd2d9d36b7948c
Brian Norris <briannorris@chromium.org>
2016-10-26 16:29:20 -0700

    UPSTREAM: mwifiex: don't do unbalanced free()'ing in cleanup_if()
    The cleanup_if() callback is the inverse of init_if(). We allocate our
    'card' interface structure in the probe() function, but we free it in
    cleanup_if(). That gives a few problems:
    (a) we leak this memory if probe() fails before we reach init_if()
    (b) we can't safely utilize 'card' after cleanup_if() -- namely, in
        remove() or suspend(), both of which might race with the cleanup
        paths in our asynchronous FW initialization path
    
    Solution: just use devm_kzalloc(), which will free this structure
    properly when the device is removed -- and drop the set_drvdata(...,
    NULL), since the driver core does this for us. This also removes the
    temptation to use drvdata == NULL as a hack for checking if the device
    has been "cleaned up."
    
    I *do* leave the set_drvdata(..., NULL) for the hacky SDIO
    mwifiex_recreate_adapter(), since the device core won't be able to clear
    that one for us.
    
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    (cherry picked from commit 66b9c182538e2ed11d31120e853321e4ea6f3e5a)
    
    BUG=b:36466135
    TEST=check wifi still works on kevin
    
    Change-Id: I14450551276d1475e7a0cf35f419ff481cda1a23
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/462194
    Tested-by: Brian Norris <briannorris@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>

--- drivers/net/wireless/marvell/mwifiex/usb.c
+++ drivers/net/wireless/marvell/mwifiex/usb.c
@@ -382,7 +382,7 @@ static int mwifiex_usb_probe(struct usb_interface *intf,
 	struct usb_card_rec *card;
 	u16 id_vendor, id_product, bcd_device;
 
-	card = kzalloc(sizeof(struct usb_card_rec), GFP_KERNEL);
+	card = devm_kzalloc(&intf->dev, sizeof(*card), GFP_KERNEL);
 	if (!card)
 		return -ENOMEM;
 
@@ -479,7 +479,6 @@ static int mwifiex_usb_probe(struct usb_interface *intf,
 	if (ret) {
 		pr_err("%s: mwifiex_add_card failed: %d\n", __func__, ret);
 		usb_reset_device(udev);
-		kfree(card);
 		return ret;
 	}
 
@@ -639,11 +638,7 @@ static void mwifiex_usb_disconnect(struct usb_interface *intf)
 		    "%s: removing card\n", __func__);
 	mwifiex_remove_card(adapter, &add_remove_card_sem);
 
-	usb_set_intfdata(intf, NULL);
 	usb_put_dev(interface_to_usbdev(intf));
-	kfree(card);
-
-	return;
 }
 
 static struct usb_driver mwifiex_usb_driver = {

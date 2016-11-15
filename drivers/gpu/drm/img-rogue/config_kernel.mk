6159a24142c4a4a08fdda435464907a48b2db076
Frank Binns <frank.binns@imgtec.com>
2016-11-15 15:00:00 +0000

    CHROMIUM: drm/img-rogue: Add 1.7 IMG PowerVR Rogue driver
    Add 1.7@4304113 release of the PowerVR Rogue driver.
    
    Signed-off-by: Frank Binns <frank.binns@imgtec.com>
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
    [djkurtz: For chromeos-4.4, just add 1.7 support directly without 1.6.]
    
    BUG=chrome-os-partner:58144
    TEST=Boot to UI
    
    Reviewed-on: https://chromium-review.googlesource.com/411824
    
    Change-Id: I6c337df1084b6710185ca0d14a3195bbb48e22b7
    Reviewed-on: https://chromium-review.googlesource.com/424014
    Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

---
+++ drivers/gpu/drm/img-rogue/config_kernel.mk
@@ -0,0 +1,27 @@
+override PVRSRV_MODNAME := pvrsrvkm
+override PVR_BUILD_DIR := mt8173_linux
+override PVR_DVFS := 1
+override PVR_HANDLE_BACKEND := idr
+override PVR_SYSTEM := mt8173
+override SUPPORT_BUFFER_SYNC := 1
+override SUPPORT_COMPUTE := 1
+override SUPPORT_ES32 := 1
+override SUPPORT_GPUTRACE_EVENTS := 1
+override SUPPORT_KERNEL_SRVINIT := 1
+override SUPPORT_SERVER_SYNC := 1
+override SUPPORT_TLA := 1
+ifeq ($(CONFIG_DRM_POWERVR_ROGUE_DEBUG),y)
+override BUILD := debug
+override PVR_BUILD_TYPE := debug
+override PVR_RI_DEBUG := 1
+override PVR_SERVICES_DEBUG := 1
+override SUPPORT_DEVICEMEMHISTORY_BRIDGE := 1
+override SUPPORT_PAGE_FAULT_DEBUG := 1
+else
+override BUILD := release
+override PVR_BUILD_TYPE := release
+endif
+ifeq ($(CONFIG_DRM_POWERVR_ROGUE_PDUMP),y)
+override EXTRA_PVRSRVKM_COMPONENTS := dbgdrv
+override PDUMP := 1
+endif

16b1f11a4ec19ecd550628356cf754676a7bf363
Sonny Rao <sonnyrao@chromium.org>
2016-12-05 17:52:36 -0800

    Revert "CHROMIUM: sched: Add over-utilization/tipping point indicator"
    This reverts commit f2896eb51bb044b23fc78ed00c5b28b85f8a675f.
    
    BUG=chrome-os-partner:58667
    TEST=build/boot on kevin
    
    Change-Id: I341b354e710efd6711d584c7e77cd3d5d6e0b8ce
    Reviewed-on: https://chromium-review.googlesource.com/419965
    Commit-Ready: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4228,8 +4228,6 @@ static inline void hrtick_update(struct rq *rq)
 }
 #endif
 
-static bool cpu_overutilized(int cpu);
-
 /*
  * The enqueue_task method is called before nr_running is
  * increased. Here we update the fair scheduling stats and
@@ -4240,7 +4238,6 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 {
 	struct cfs_rq *cfs_rq;
 	struct sched_entity *se = &p->se;
-	int task_new = !(flags & ENQUEUE_WAKEUP);
 
 	for_each_sched_entity(se) {
 		if (se->on_rq)
@@ -4272,12 +4269,9 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		update_cfs_shares(cfs_rq);
 	}
 
-	if (!se) {
+	if (!se)
 		add_nr_running(rq, 1);
-		if (!task_new && !rq->rd->overutilized &&
-		    cpu_overutilized(rq->cpu))
-			rq->rd->overutilized = true;
-	}
+
 	hrtick_update(rq);
 }
 
@@ -6487,12 +6481,11 @@ group_type group_classify(struct sched_group *group,
  * @local_group: Does group contain this_cpu.
  * @sgs: variable to hold the statistics for this group.
  * @overload: Indicate more than one runnable task for any CPU.
- * @overutilized: Indicate overutilization for any CPU.
  */
 static inline void update_sg_lb_stats(struct lb_env *env,
 			struct sched_group *group, int load_idx,
 			int local_group, struct sg_lb_stats *sgs,
-			bool *overload, bool *overutilized)
+			bool *overload)
 {
 	unsigned long load;
 	int i, nr_running;
@@ -6526,9 +6519,6 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 		 */
 		if (!nr_running && idle_cpu(i))
 			sgs->idle_cpus++;
-
-		if (cpu_overutilized(i))
-			*overutilized = true;
 	}
 
 	/* Adjust by relative CPU capacity of the group */
@@ -6634,7 +6624,7 @@ static inline void update_sd_lb_stats(struct lb_env *env, struct sd_lb_stats *sd
 	struct sched_group *sg = env->sd->groups;
 	struct sg_lb_stats tmp_sgs;
 	int load_idx, prefer_sibling = 0;
-	bool overload = false, overutilized = false;
+	bool overload = false;
 
 	if (child && child->flags & SD_PREFER_SIBLING)
 		prefer_sibling = 1;
@@ -6656,7 +6646,7 @@ static inline void update_sd_lb_stats(struct lb_env *env, struct sd_lb_stats *sd
 		}
 
 		update_sg_lb_stats(env, sg, load_idx, local_group, sgs,
-						&overload, &overutilized);
+						&overload);
 
 		if (local_group)
 			goto next_group;
@@ -6700,14 +6690,8 @@ next_group:
 		/* update overload indicator if we are at root domain */
 		if (env->dst_rq->rd->overload != overload)
 			env->dst_rq->rd->overload = overload;
-
-		/* Update over-utilization (tipping point, U >= 0) indicator */
-		if (env->dst_rq->rd->overutilized != overutilized)
-			env->dst_rq->rd->overutilized = overutilized;
-	} else {
-		if (!env->dst_rq->rd->overutilized && overutilized)
-			env->dst_rq->rd->overutilized = true;
 	}
+
 }
 
 /**
@@ -8100,9 +8084,6 @@ static void task_tick_fair(struct rq *rq, struct task_struct *curr, int queued)
 
 	if (static_branch_unlikely(&sched_numa_balancing))
 		task_tick_numa(rq, curr);
-
-	if (!rq->rd->overutilized && cpu_overutilized(task_cpu(curr)))
-		rq->rd->overutilized = true;
 }
 
 /*

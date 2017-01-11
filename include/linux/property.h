926c9afedb294049bce5cf5419f7a5bbee6527ad
Furquan Shaikh <furquan@chromium.org>
2017-01-10 19:50:29 -0800

    CHROMIUM: drivers: property: Export dev_fwnode
    Export dev_fwnode so that it can be used by other drivers like regulator.
    
    BUG=chrome-os-partner:60194
    BRANCH=None
    TEST=Compiles successfully.
    
    Change-Id: Ic304aacd1b7b41905462f07ade7c11610c8d9875
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/426307
    Reviewed-by: Benson Leung <bleung@chromium.org>

--- include/linux/property.h
+++ include/linux/property.h
@@ -49,6 +49,7 @@ int device_property_read_string(struct device *dev, const char *propname,
 int device_property_match_string(struct device *dev,
 				 const char *propname, const char *string);
 
+struct fwnode_handle *dev_fwnode(struct device *dev);
 bool fwnode_property_present(struct fwnode_handle *fwnode, const char *propname);
 int fwnode_property_read_u8_array(struct fwnode_handle *fwnode,
 				  const char *propname, u8 *val,

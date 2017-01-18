e2414ce70fbdfd400f210588b1f32945a4dbeda4
Guenter Roeck <groeck@chromium.org>
2017-01-18 11:40:18 -0800

    Revert "FROMLIST: debug: More properly delay for secondary CPUs"
    This reverts commit 1705292afc1fd5a57c8866dbbf68b2d464c31396.
    
    Prepare for merge of v4.4.44 into chromeos-4.4: Drop local patch to pick up
    patch from upstream instead.
    
    BUG=chromium-682347
    TEST=Combined with merge, build and test on Kevin, Gru, Reef, Chell with 4.4 kernel
    
    Change-Id: Iac70b1db22e7cc5755a4d1ed0abac3eed1dbb1b8
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- kernel/debug/debug_core.c
+++ kernel/debug/debug_core.c
@@ -598,11 +598,11 @@ return_normal:
 	/*
 	 * Wait for the other CPUs to be notified and be waiting for us:
 	 */
-	time_left = DIV_ROUND_UP(loops_per_jiffy * HZ, 100);
+	time_left = loops_per_jiffy * HZ;
 	while (kgdb_do_roundup && --time_left &&
 	       (atomic_read(&masters_in_kgdb) + atomic_read(&slaves_in_kgdb)) !=
 		   online_cpus)
-		__delay(100);
+		cpu_relax();
 	if (!time_left)
 		pr_crit("Timed out waiting for secondary CPUs.\n");
 

6d909574d882daff24fd68089c87b309d5a3bc8f
Douglas Anderson <dianders@chromium.org>
2017-01-19 14:30:38 -0800

    CHROMIUM: drm: bridge/analogix: Properly disable aux chan retries on rk
    The comments in analogix_dp_init_aux() claim that we're disabling aux
    channel retries, but then right below it for Rockchip it sets them to
    3.  If we actually need 3 retries for Rockchip then we could adjust
    the comment, but it seems more likely that we want the same retry
    behavior across all platforms.
    
    BUG=chrome-os-partner:62156
    TEST=Boot and eDP still works; s2r and eDP still works
    
    Change-Id: If0c1c624c426de6899fb8c79a87dffac91715a9a
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/430381
    Tested-by: 征增 王 <wzz@rock-chips.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: 征增 王 <wzz@rock-chips.com>

--- drivers/gpu/drm/bridge/analogix/analogix_dp_reg.c
+++ drivers/gpu/drm/bridge/analogix/analogix_dp_reg.c
@@ -480,15 +480,16 @@ void analogix_dp_init_aux(struct analogix_dp_device *dp)
 
 	analogix_dp_reset_aux(dp);
 
-	/* Disable AUX transaction H/W retry */
-	if (dp->plat_data && (dp->plat_data->dev_type == ROCKCHIP_DP))
-		reg = AUX_BIT_PERIOD_EXPECTED_DELAY(0) |
-		      AUX_HW_RETRY_COUNT_SEL(3) |
-		      AUX_HW_RETRY_INTERVAL_600_MICROSECONDS;
+	/* AUX_BIT_PERIOD_EXPECTED_DELAY doesn't apply to Rockchip IP */
+	if (dp->plat_data && (dp->plat_data->dev_type != ROCKCHIP_DP))
+		reg = AUX_BIT_PERIOD_EXPECTED_DELAY(3);
 	else
-		reg = AUX_BIT_PERIOD_EXPECTED_DELAY(3) |
-		      AUX_HW_RETRY_COUNT_SEL(0) |
-		      AUX_HW_RETRY_INTERVAL_600_MICROSECONDS;
+		reg = 0;
+
+	/* Disable AUX transaction H/W retry */
+	reg |= AUX_HW_RETRY_COUNT_SEL(0) |
+	       AUX_HW_RETRY_INTERVAL_600_MICROSECONDS;
+
 	writel(reg, dp->reg_base + ANALOGIX_DP_AUX_HW_RETRY_CTL);
 
 	/* Receive AUX Channel DEFER commands equal to DEFFER_COUNT*64 */

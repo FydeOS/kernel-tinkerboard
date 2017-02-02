3192d6c396d799c3e87647dd391101c36e354163
Shanth Murthy <shanth.murthy@intel.com>
2017-02-01 22:15:34 -0800

    FROMLIST: platform/x86: intel_pmc_ipc: read S0ix residency API
    This patch adds a new API to indicate S0ix residency in usec. It utilizes
    the PMC Global Control Registers (GCR) to read deep and shallow
    S0ix residency.
    
    PMC MMIO resources:
            o Lower 4kB: IPC1 (PMC inter-processor communication) interface
            o Upper 4kB: GCR (Global Control Registers)
    
    This enables the power management framework to take corrective actions when
    the platform fails to enter S0ix after kernel freeze as part of the suspend
    to idle flow. (echo freeze > /sys/power/state).
    
    This is expected to be used with a S0ix failsafe framework such as:
    <https://lwn.net/Articles/689505/>
    
    BUG=chrome-os-partner:57313
    TEST=Check the API (intel_pmc_read_s0ix_residency) by calling it from
    other drivers that are used in the framework to detect S0ix failures.
    
    Change-Id: I8b60a97aa871fd06749cca06e43452b0d83dd626
    Signed-off-by: Shanth Murthy <shanth.murthy@intel.com>
    Signed-off-by: Rajneesh Bhardwaj <rajneesh.bhardwaj@intel.com>
    [andy: fixed kbuild error, removed "total" from variables, fixed macro]
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    (am from https://lkml.org/lkml/2017/2/13/282)
    Reviewed-on: https://chromium-review.googlesource.com/435065
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Benson Leung <bleung@chromium.org>

--- drivers/platform/x86/intel_pmc_ipc.c
+++ drivers/platform/x86/intel_pmc_ipc.c
@@ -32,6 +32,8 @@
 #include <linux/notifier.h>
 #include <linux/suspend.h>
 #include <linux/acpi.h>
+#include <linux/io-64-nonatomic-lo-hi.h>
+
 #include <asm/intel_pmc_ipc.h>
 #include <linux/platform_data/itco_wdt.h>
 
@@ -53,6 +55,18 @@
 #define IPC_DPTR		0x0C
 #define IPC_WRITE_BUFFER	0x80
 #define IPC_READ_BUFFER		0x90
+
+/* PMC Global Control Registers */
+#define GCR_TELEM_DEEP_S0IX_OFFSET	0x1078
+#define GCR_TELEM_SHLW_S0IX_OFFSET	0x1080
+
+/* Residency with clock rate at 19.2MHz to usecs */
+#define S0IX_RESIDENCY_IN_USECS(d, s)		\
+({						\
+	u64 result = 10ull * ((d) + (s));	\
+	do_div(result, 192);			\
+	result;					\
+})
 
 /*
  * 16-byte buffer for sending data associated with IPC command.
@@ -112,6 +126,7 @@ static struct intel_pmc_ipc_dev {
 	/* gcr */
 	resource_size_t gcr_base;
 	int gcr_size;
+	bool has_gcr_regs;
 
 	/* punit */
 	struct platform_device *punit_dev;
@@ -175,6 +190,11 @@ static inline u8 ipc_data_readb(u32 offset)
 static inline u32 ipc_data_readl(u32 offset)
 {
 	return readl(ipcdev.ipc_base + IPC_READ_BUFFER + offset);
+}
+
+static inline u64 gcr_data_readq(u32 offset)
+{
+	return readq(ipcdev.ipc_base + offset);
 }
 
 static int intel_pmc_ipc_check_status(void)
@@ -749,7 +769,8 @@ static int ipc_plat_get_res(struct platform_device *pdev)
 		dev_err(&pdev->dev, "Failed to get ipc resource\n");
 		return -ENXIO;
 	}
-	size = PLAT_RESOURCE_IPC_SIZE;
+	size = PLAT_RESOURCE_IPC_SIZE + PLAT_RESOURCE_GCR_SIZE;
+
 	if (!request_mem_region(res->start, size, pdev->name)) {
 		dev_err(&pdev->dev, "Failed to request ipc resource\n");
 		return -EBUSY;
@@ -762,7 +783,7 @@ static int ipc_plat_get_res(struct platform_device *pdev)
 	}
 	ipcdev.ipc_base = addr;
 
-	ipcdev.gcr_base = res->start + size;
+	ipcdev.gcr_base = res->start + PLAT_RESOURCE_IPC_SIZE;
 	ipcdev.gcr_size = PLAT_RESOURCE_GCR_SIZE;
 	dev_info(&pdev->dev, "ipc res: %pR\n", res);
 
@@ -784,6 +805,31 @@ static int ipc_plat_get_res(struct platform_device *pdev)
 
 	return 0;
 }
+
+/**
+ * intel_pmc_s0ix_counter_read() - Read S0ix residency.
+ * @data: Out param that contains current S0ix residency count.
+ *
+ * Return: an error code or 0 on success.
+ */
+int intel_pmc_s0ix_counter_read(u64 *data)
+{
+	u64 deep, shlw;
+
+	if (!ipcdev.has_gcr_regs)
+		return -EACCES;
+
+	/* Broxton SoC supports deep and shallow S0ix states, hence the total
+	 * S0ix residency is a sum of both deep and shallow S0ix states.
+	 */
+	deep = gcr_data_readq(GCR_TELEM_DEEP_S0IX_OFFSET);
+	shlw = gcr_data_readq(GCR_TELEM_SHLW_S0IX_OFFSET);
+
+	*data = S0IX_RESIDENCY_IN_USECS(deep, shlw);
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(intel_pmc_s0ix_counter_read);
 
 #ifdef CONFIG_ACPI
 static const struct acpi_device_id ipc_acpi_ids[] = {
@@ -833,6 +879,8 @@ static int ipc_plat_probe(struct platform_device *pdev)
 		goto err_sys;
 	}
 
+	ipcdev.has_gcr_regs = true;
+
 	return 0;
 err_sys:
 	free_irq(ipcdev.irq, &ipcdev);
@@ -844,8 +892,11 @@ err_device:
 	iounmap(ipcdev.ipc_base);
 	res = platform_get_resource(pdev, IORESOURCE_MEM,
 				    PLAT_RESOURCE_IPC_INDEX);
-	if (res)
-		release_mem_region(res->start, PLAT_RESOURCE_IPC_SIZE);
+	if (res) {
+		release_mem_region(res->start,
+				   PLAT_RESOURCE_IPC_SIZE +
+				   PLAT_RESOURCE_GCR_SIZE);
+	}
 	return ret;
 }
 
@@ -861,8 +912,11 @@ static int ipc_plat_remove(struct platform_device *pdev)
 	iounmap(ipcdev.ipc_base);
 	res = platform_get_resource(pdev, IORESOURCE_MEM,
 				    PLAT_RESOURCE_IPC_INDEX);
-	if (res)
-		release_mem_region(res->start, PLAT_RESOURCE_IPC_SIZE);
+	if (res) {
+		release_mem_region(res->start,
+				   PLAT_RESOURCE_IPC_SIZE +
+				   PLAT_RESOURCE_GCR_SIZE);
+	}
 	ipcdev.dev = NULL;
 	return 0;
 }

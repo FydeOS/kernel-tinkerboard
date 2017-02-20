c5feabbd333a20d9419e80bca99986d8c892c029
Wei-Ning Huang <wnhuang@google.com>
2017-02-20 15:19:15 +0800

    FIXUP: CHROMIUM: Input: centroiding: add Google centroiding touchpad driver
    Fixup extra comments after the CL merged.
    
    BUG=chrome-os-partner:61088
    TEST=`emerge-eve chromeos-kernel-4_4`
    
    Change-Id: Ib15d8dbba86b82b85de117ed5e50c85bd03190c4
    Reviewed-on: https://chromium-review.googlesource.com/445066
    Commit-Ready: Wei-Ning Huang <wnhuang@chromium.org>
    Tested-by: Wei-Ning Huang <wnhuang@chromium.org>
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>

--- drivers/input/mouse/centroiding.c
+++ drivers/input/mouse/centroiding.c
@@ -6,6 +6,7 @@
  * Copyright 2017 Google Inc.
  */
 
+#include <asm/unaligned.h>
 #include <linux/acpi.h>
 #include <linux/device.h>
 #include <linux/i2c.h>
@@ -20,7 +21,7 @@
 #define DRIVER_NAME		"centroiding"
 #define DRIVER_VERSION		"0.0.1"
 #define GOOGLE_VENDOR_ID	0x18d1
-#define GOOGLE_CENTROIDING_PRODUCT_ID	0x6365
+#define GOOGLE_CENTROIDING_PRODUCT_ID	0x5028
 
 #define CENTROIDING_MAX_FINGERS 10
 #define CENTROIDING_MAX_X	1920
@@ -37,18 +38,19 @@ struct centroiding_data {
 };
 
 struct point_data {
-	int16_t id;
-	uint16_t x;
-	uint16_t y;
-	uint8_t amp;
+	__le16 id;
+	__le16 x, y;
+	u8 amp;
 } __packed;
 
 struct report_data {
-	uint8_t button_down;
-	uint8_t contact_num;
+	u8 button_down;
+	u8 contact_num;
 	struct point_data data[CENTROIDING_MAX_FINGERS];
 } __packed;
 
+
+static struct report_data report;
 
 static irqreturn_t centroiding_isr(int irq, void *dev_id)
 {
@@ -59,18 +61,8 @@ static irqreturn_t centroiding_isr(int irq, void *dev_id)
 	int ret;
 	int i;
 
-	struct report_data report;
-	struct i2c_msg msgs[] = {
-		{
-			.addr = client->addr,
-			.flags = client->flags | I2C_M_RD,
-			.len = sizeof(report),
-			.buf = (uint8_t *)&report,
-		}
-	};
-
-	ret = i2c_transfer(client->adapter, msgs, ARRAY_SIZE(msgs));
-	if (ret != ARRAY_SIZE(msgs)) {
+	ret = i2c_master_recv(client, (char *)&report, sizeof(report));
+	if (ret != sizeof(report)) {
 		dev_err(dev, "err %d: failed to read packet from i2c\n", ret);
 		return IRQ_HANDLED;
 	}
@@ -78,12 +70,15 @@ static irqreturn_t centroiding_isr(int irq, void *dev_id)
 	for (i = 0; i < CENTROIDING_MAX_FINGERS; i++) {
 		struct point_data *point = &report.data[i];
 
-		if (point->id != -1) {
+		if ((s16)le16_to_cpu(point->id) != -1) {
 			input_mt_slot(input, i);
 			input_mt_report_slot_state(input, MT_TOOL_FINGER, true);
-			input_report_abs(input, ABS_MT_POSITION_X, point->x);
-			input_report_abs(input, ABS_MT_POSITION_Y, point->y);
-			input_report_abs(input, ABS_MT_PRESSURE, point->amp);
+			input_report_abs(input, ABS_MT_POSITION_X,
+					 le16_to_cpu(point->x));
+			input_report_abs(input, ABS_MT_POSITION_Y,
+					 le16_to_cpu(point->y));
+			input_report_abs(input, ABS_MT_PRESSURE,
+					 point->amp);
 		} else {
 			input_mt_slot(input, i);
 			input_mt_report_slot_state(input, MT_TOOL_FINGER,
@@ -92,7 +87,6 @@ static irqreturn_t centroiding_isr(int irq, void *dev_id)
 	}
 
 	input_report_key(input, BTN_LEFT, report.button_down);
-	input_mt_report_pointer_emulation(input, true);
 	input_sync(input);
 
 	return IRQ_HANDLED;
@@ -114,27 +108,10 @@ static int centroiding_setup_input_devices(struct centroiding_data *data)
 	input->id.product = GOOGLE_CENTROIDING_PRODUCT_ID;
 	input_set_drvdata(input, data);
 
-	error = input_mt_init_slots(input, CENTROIDING_MAX_FINGERS,
-				    INPUT_MT_POINTER | INPUT_MT_DROP_UNUSED);
-	if (error) {
-		dev_err(dev, "failed to initialize MT slots: %d\n", error);
-		return error;
-	}
-
-	__set_bit(EV_ABS, input->evbit);
-	__set_bit(INPUT_PROP_POINTER, input->propbit);
 	__set_bit(INPUT_PROP_BUTTONPAD, input->propbit);
-	__set_bit(BTN_LEFT, input->keybit);
+	input_set_capability(input, EV_KEY, BTN_LEFT);
 
-	/* Set up ST parameters */
-	input_set_abs_params(input, ABS_X, 0, CENTROIDING_MAX_X, 0, 0);
-	input_set_abs_params(input, ABS_Y, 0, CENTROIDING_MAX_Y, 0, 0);
-	input_abs_set_res(input, ABS_X, CENTROIDING_X_RES);
-	input_abs_set_res(input, ABS_Y, CENTROIDING_Y_RES);
-	input_set_abs_params(input, ABS_PRESSURE, 0,
-			     CENTROIDING_MAX_PRESSURE, 0, 0);
-
-	/* And MT parameters */
+	/* Set up MT parameters */
 	input_set_abs_params(input, ABS_MT_POSITION_X, 0,
 			     CENTROIDING_MAX_X, 0, 0);
 	input_set_abs_params(input, ABS_MT_POSITION_Y, 0,
@@ -143,6 +120,13 @@ static int centroiding_setup_input_devices(struct centroiding_data *data)
 	input_abs_set_res(input, ABS_MT_POSITION_Y, CENTROIDING_Y_RES);
 	input_set_abs_params(input, ABS_MT_PRESSURE, 0,
 			     CENTROIDING_MAX_PRESSURE, 0, 0);
+
+	error = input_mt_init_slots(input, CENTROIDING_MAX_FINGERS,
+				    INPUT_MT_POINTER);
+	if (error) {
+		dev_err(dev, "failed to initialize MT slots: %d\n", error);
+		return error;
+	}
 
 	data->input = input;
 
@@ -160,15 +144,6 @@ static int centroiding_probe(struct i2c_client *client,
 	if (!data)
 		return -ENOMEM;
 
-	error = devm_request_threaded_irq(&client->dev, client->irq,
-					  NULL, centroiding_isr,
-					  IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
-					  client->name, data);
-	if (error) {
-		dev_err(&client->dev, "cannot register irq=%d\n", client->irq);
-		return error;
-	}
-
 	i2c_set_clientdata(client, data);
 	data->client = client;
 
@@ -178,6 +153,15 @@ static int centroiding_probe(struct i2c_client *client,
 	if (error) {
 		dev_err(&client->dev, "failed to register input device: %d\n",
 			error);
+		return error;
+	}
+
+	error = devm_request_threaded_irq(&client->dev, client->irq,
+					  NULL, centroiding_isr,
+					  IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
+					  client->name, data);
+	if (error) {
+		dev_err(&client->dev, "cannot register irq=%d\n", client->irq);
 		return error;
 	}
 

aaf98240335e91fa82840c81b41685bfed49343f
Jeffy Chen <jeffy.chen@rock-chips.com>
2017-02-24 14:24:31 +0800

    UPSTREAM: mwifiex: wake system up when receives a wake irq
    Currrently we are disabling this wake irq after receiving it. If this
    happens before we finish suspend and the pm event check is disabled,
    the system will continue suspending, and this irq would not work again.
    
    We may need to abort system suspend to avoid that.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    (cherry picked from git://git.kernel.org/pub/scm/linux/kernel/git/kvalo/wireless-drivers-next.git master
     commit ef7e0714742541dcce75b646b64f7a376b7d4368)
    
    BUG=b:36466135
    TEST=check wifi still works on kevin
    
    Change-Id: I6a6c24f31e0d88a6eeb5daa5afac42c5c78b253d
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/462227
    Tested-by: Brian Norris <briannorris@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>

--- drivers/net/wireless/marvell/mwifiex/main.c
+++ drivers/net/wireless/marvell/mwifiex/main.c
@@ -17,6 +17,8 @@
  * this warranty disclaimer.
  */
 
+#include <linux/suspend.h>
+
 #include "main.h"
 #include "wmm.h"
 #include "cfg80211.h"
@@ -1535,6 +1537,7 @@ static irqreturn_t mwifiex_irq_wakeup_handler(int irq, void *priv)
 
 	/* Notify PM core we are wakeup source */
 	pm_wakeup_event(adapter->dev, 0);
+	pm_system_wakeup();
 
 	return IRQ_HANDLED;
 }
